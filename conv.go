6cd73929134a1b22707b058b7f396d54169004a1
arieljuarezdev <arieljuarez.d@gmail.com>
2024-09-01 10:54:04 -0300

    is finished, need some details, but is good

--- conv.go
+++ conv.go
@@ -34,6 +34,7 @@ func main() {
 			switch {
 			case sca_ini == "f" || sca_ini == "F":
 				sca_ini := "f"
+				fmt.Println("*-*-*-*-*-*-*-*-*-*-*-*-*-")
 				fmt.Print("Digite a temperatura a ser convertida em °F: ")
 				fmt.Scanf("%f", &temp_init)
 				// enviar por meio do paramentro o valor de escala inicial, temperatura inicial e escala a converter.
@@ -41,6 +42,7 @@ func main() {
 				break
 			case sca_ini == "c" || sca_ini == "C":
 				sca_ini := "c"
+				fmt.Println("*-*-*-*-*-*-*-*-*-*-*-*-*-")
 				fmt.Print("Digite a temperatura a ser convertida em °C: ")
 				fmt.Scanf("%f", &temp_init)
 				// enviar por meio do paramentro o valor de escala inicial, temperatura inicial e escala a converter.
@@ -48,6 +50,7 @@ func main() {
 				break
 			case sca_ini == "k" || sca_ini == "K":
 				sca_ini := "k"
+				fmt.Println("*-*-*-*-*-*-*-*-*-*-*-*-*-")
 				fmt.Print("Digite a temperatura a ser convertida em K: ")
 				fmt.Scanf("%f", &temp_init)
 				// enviar por meio do paramentro o valor de escala inicial, temperatura inicial e escala a converter.
@@ -57,6 +60,7 @@ func main() {
 		}
 
 	} else {
+		fmt.Println("*-*-*-*-*-*-*-*-*-*-*-*-*-")
 		fmt.Println("erro: digite o LETRA CORRESPONDENTE A ESCALA SOLICITADA")
 		return
 	}
@@ -68,31 +72,45 @@ func conv_temp(s_i, s_c string, t_i float64) {
 	//tratamento da escala inicial (to chapadasso, isso não bom)
 	switch {
 	case s_c == "f" || s_c == "F":
-		// K || C
 		if s_i == "k" {
 			// conversão Kelvin p/ Fahrenheit
 			t_c := (((t_i - 273.15) * 9) / 5) + 32
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 			fmt.Printf("%.2f K em °F, consiste em: %.2f", t_i, t_c)
 		} else {
 			// conversão Celsius p/ Fahrenheit
 			t_c := ((t_i * 9) / 5) + 32
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 			fmt.Printf("%.2f °C em °F, consiste em: %.2f", t_i, t_c)
 		}
 		break
 	case s_c == "c" || s_c == "C":
 		// K || F
-		if s_c == "k" {
+		if s_i == "k" {
+			// Conversão de Kelvin p/ Celsius
 			t_c := t_i - 273.15
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 			fmt.Printf("%.2f K em °C, consiste em: %2.f", t_i, t_c)
+		} else {
+			// Conversão de Fahrenheit p/ Celsius
+			t_c := (t_i - 32) * 5 / 9
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+			fmt.Printf("%.2f °F em °C, consiste em: %2.f", t_i, t_c)
 		}
-		// fmt.Print("Entrada em °C: ")
 		break
 	case s_c == "k" || s_c == "K":
 		// F || C
-
-		// chapado fiz errado kkkk
-		t_c := (((t_i - 32) * 5) / 9) + 273.15
-		fmt.Printf("%.2f °F em K, consiste em: %.2f", t_i, t_c)
+		if s_i == "f" {
+			// Conversão de Fahrenheit p/ Kelvin
+			t_c := (((t_i - 32) * 5) / 9) + 273.15
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+			fmt.Printf("%.2f °F em K, consiste em: %.2f", t_i, t_c)
+		} else {
+			// Conversão de Fahrenheit p/ Celsius
+			t_c := t_i + 273.15
+			fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+			fmt.Printf("%.2f °C em K, consiste em: %.2f", t_i, t_c)
+		}
 		break
 	}
 }
